7bb4d786034fbd926243d9e3876ed4cf10383824
DPS2004 <dpsophinos@gmail.com>
2020-10-18 18:05:27 -0400

    pulls cards from cards.json now

--- main.lua
+++ main.lua
@@ -3,6 +3,22 @@ local client = discordia.Client()
 local prefix = "c!"
 local privatestuff = require('privatestuff')
 
+
+
+json = require('libs/json')
+cj =  io.open("cards.json", "r")
+cdata = json.decode(cj:read("*a"))
+--generate pull table
+ptable = {}
+for i,v in ipairs(cdata.groups) do
+  for w,x in ipairs(v.cards) do
+    print(x.name.. "loaded")
+    for y=1,(cdata.basemult*v.basechance*x.chance) do
+      table.insert(ptable,x.name)
+    end
+  end
+end
+
 client:on('ready', function()
 	print('Logged in as '.. client.user.username)
 end)
@@ -13,11 +29,13 @@ client:on('messageCreate', function(message)
     print("someone did !ping")
 	end
 	if message.content == prefix..'pull' then
-		message.channel:send('haha wowie! discord user '.. message.member.mentionString .. ' whos discord ID happens to be ' .. message.member.user.id ..' you got a card good job my broski')
-    if message.member.user.id == '300050030923087872' then
-      message.channel:send('also since you are huantian (i think?) h e r e  a r e  s  o  m  e   e   x   t   r   a        s    p    a    c    e    s')
-    end
+		--message.channel:send('haha wowie! discord user '.. message.member.mentionString .. ' whos discord ID happens to be ' .. message.member.user.id ..' you got a card good job my broski')
     print("someone did !pull")
+    message.channel:send('Pulling card...')
+    local newcard = ptable[math.random(#ptable)]
+    message.channel:send('Woah! '.. message.member.mentionString ..' got a **'.. newcard ..'!** The **'.. newcard ..'** has been added to their inventory.')
+
+    
 	end
 end)
 
