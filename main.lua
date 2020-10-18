23b241035fa8c31b008c6204b0b89d2b274dd2b9
DPS2004 <dpsophinos@gmail.com>
2020-10-18 16:53:58 -0400

    MORE

--- main.lua
+++ main.lua
@@ -13,8 +13,11 @@ client:on('messageCreate', function(message)
     print("someone did !ping")
 	end
 	if message.content == prefix..'pull' then
-		message.channel:send('haha wowie you got a card good job my broski')
-    print("someone did !ping")
+		message.channel:send('haha wowie! discord user '.. message.member.mentionString .. ' whos discord ID happens to be ' .. message.member.user.id ..' you got a card good job my broski')
+    if message.member.user.id == '300050030923087872' then
+      message.channel:send('also since you are huantian (i think?) h e r e  a r e  s  o  m  e   e   x   t   r   a        s    p    a    c    e    s')
+    end
+    print("someone did !pull")
 	end
 end)
 
