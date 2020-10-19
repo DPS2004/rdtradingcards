7ed3a9d300adf64d6cbc80dfe88aefd2be6354cb
DPS2004 <dpsophinos@gmail.com>
2020-10-19 13:55:53 -0400

    update show command to account for names

--- main.lua
+++ main.lua
@@ -14,11 +14,36 @@ cdata = json.decode(cj:read("*a"))
 cj:close()
 --generate pull table
 ptable = {}
+cdb = {}
 for i,v in ipairs(cdata.groups) do
   for w,x in ipairs(v.cards) do
-    print(x.name.. " loaded")
+    
     for y=1,(cdata.basemult*v.basechance*x.chance) do
       table.insert(ptable,x.filename)
+    end
+    table.insert(cdb,x)
+    print(x.name.. " loaded!")
+  end
+end
+
+
+function fntoname(x)
+  print("finding "..x)
+  for i,v in ipairs(cdb) do
+    if string.lower(v.filename) == string.lower(x) then
+      local match = v.name
+      print(x.." = "..v.name)
+      return v.name
+    end
+  end
+  
+end
+
+function nametofn(x)
+  for i,v in ipairs(cdb) do
+    if string.lower(v.name) == string.lower(x) then
+      local match = v.filename
+      return v.filename
     end
   end
 end
@@ -30,15 +55,17 @@ end)
 client:on('messageCreate', function(message)
 	if message.content == prefix..'ping' then
 		message.channel:send('pong')
-    print("someone did !ping")
+    print(message.member.name .. " did !ping")
 	end
 	if message.content == prefix..'pull' then
 		--message.channel:send('haha wowie! discord user '.. message.member.mentionString .. ' whos discord ID happens to be ' .. message.member.user.id ..' you got a card good job my broski')
-    print("someone did !pull")
+    print(message.member.name .. " did !pull")
     message.channel:send('Pulling card...')
     local newcard = ptable[math.random(#ptable)]
+    ncn = fntoname(newcard)
+    print(ncn)
     message.channel:send {
-        content = 'Woah! '.. message.member.mentionString ..' got a **'.. newcard ..'!** The **'.. newcard ..'** card has been added to their inventory.',
+        content = 'Woah! '.. message.member.mentionString ..' got a **'.. ncn ..'!** The **'.. ncn ..'** card has been added to their inventory.',
         file = "card_images/" .. newcard .. ".png"
       }
 
@@ -55,33 +82,42 @@ client:on('messageCreate', function(message)
     
 	end
 	if message.content == prefix..'inventory' then
-    print("someone did !inventory")
+    print(message.member.name .. " did !inventory")
     uj = dpf.loadjson("savedata/" .. message.member.user.id .. ".json",{inventory={}})
     local invstring = ''
     for k,v in pairs(uj.inventory) do
-      invstring = invstring .. "**" .. k .. "** x" .. v .. "\n"
+      invstring = invstring .. "**" .. fntoname(k) .. "** x" .. v .. "\n"
     end
       
     message.channel:send("Your inventory contains:\n" .. invstring)
 	end
-  if message.content:find(prefix.. 'show') then
-    print("someone did !show")
+  if message.content:find(prefix.. 'show ') then
+    print(message.member.name .. " did !show")
     uj = dpf.loadjson("savedata/" .. message.member.user.id .. ".json",{inventory={}})
-    local msg = {}
-    for s in message.content:gmatch("%S+") do
-      table.insert(msg, s)
+    local request = string.sub(message.content, 8)
+    print(request)
+    local curfilename = nametofn(request)
+    if curfilename == nil then
+      curfilename = fntoname(request)
+      if curfilename ~= nil then
+        curfilename = string.lower(request)
+      end
     end
-    if #msg == 2 then
-      if uj.inventory[msg[#msg]] then
+    
+    print(curfilename)
+    if curfilename ~= nil then
+      if uj.inventory[curfilename] then
         print("user has card")
-      message.channel:send {
-        content = 'Here it is! Your '.. msg[#msg] .. ' card.',
-        file = "card_images/" .. msg[#msg] .. ".png"
-      }
+        message.channel:send {
+          content = 'Here it is! Your '.. fntoname(curfilename) .. ' card.',
+          file = "card_images/" .. curfilename .. ".png"
+        }
       else
         print("user doesnt have card")
-        message.channel:send("Sorry, but you don't have the " .. msg[#msg] .. " card in your inventory.")
+        message.channel:send("Sorry, but you don't have the " .. fntoname(curfilename) .. " card in your inventory.")
       end
+    else
+      message.channel:send("Sorry, but I could not find the " .. request .. " card in the database. Make sure that you spelled it right!")
     end
     
     
