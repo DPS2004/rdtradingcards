b47b61773f56b843a83c2b701db9965b153e5d63
DPS2004 <dpsophinos@gmail.com>
2020-12-29 23:55:17 -0500

    fix references to cards in medal command

--- commands/showmedal.lua
+++ commands/showmedal.lua
@@ -12,15 +12,15 @@ function command.run(message, mt)
       if uj.medals[curfilename] then
         print("user has medal")
         message.channel:send {
-          content = 'Here it is! Your **'.. medalfntoname(curfilename) .. '** card. The description on the back reads: ```'..medaldb[curfilename].description .. '``` The shorthand form is **' .. curfilename .. '**.',
+          content = 'Here it is! Your **'.. medalfntoname(curfilename) .. '** medal.\n The description on the back reads: ```'..medaldb[curfilename].description .. '``` The shorthand form is **' .. curfilename .. '**.',
           file = "medal_images/" .. curfilename .. ".png"
         }
       else
-        print("user doesnt have card")
-        message.channel:send("Sorry, but you don't have the **" .. medalfntoname(curfilename) .. "** card in your inventory or your storage.")
+        print("user doesnt have medal")
+        message.channel:send("Sorry, but you don't have the **" .. medalfntoname(curfilename) .. "** medal in your inventory or your storage.")
       end
     else
-      message.channel:send("Sorry, but I could not find the " .. request .. " card in the database. Make sure that you spelled it right!")
+      message.channel:send("Sorry, but I could not find the " .. request .. " medal in the database. Make sure that you spelled it right!")
     end
   
   -- whole lotta code to display a png image lmao
