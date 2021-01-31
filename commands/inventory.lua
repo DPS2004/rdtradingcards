7631465fe0aa9c6a85367513ffa6ed631184ffd3
DPS2004 <iheartscratch@gmail.com>
2021-01-31 18:11:56 -0500

    inventory ssss added

--- commands/inventory.lua
+++ commands/inventory.lua
@@ -31,7 +31,9 @@ function command.run(message, mt)
   print("maxpn " .. numkey/10)
 
   if pagenumber > maxpn then
-    pagenumber = maxpn
+    if not message.channel.id == privatestuff.specialchannelid then
+      pagenumber = maxpn
+    end
   end
   print("pagenumber " .. pagenumber)
   local invtable = {}
@@ -46,7 +48,15 @@ function command.run(message, mt)
       invstring = invstring .. invtable[i]
     end
   end
-  message.channel:send("<@".. message.author.id ..">, your inventory contains:\n" .. invstring .. "(page ".. pagenumber .. " of " .. maxpn .. ")")
+  if message.channel.id == privatestuff.specialchannelid then
+    if pagenumber == 1 then
+      message.channel:send("<@".. message.author.id ..'>, your inventory contains:\n**"I am pensiving so hard rn" - Kin** x1\n**"Rhythm Doctor is already out, We have been played for fools" - definetely_not_HIM** x1\n**"it\'s called a midlife crisis if you\'re in your 30s, right now it\'s just anxiety" - Hexakon** x1\n"**so we are being infected by space crust" - Nocallia** x1\n**"so we could literally suck on em until they\'re goo" - Donte** x1\n**"i think we delayed the game too long and theyre angry" - fizzd** x1\n(page 1 of 2)')
+    else
+      message.channel:send("<@".. message.author.id ..'>, your inventory contains:\n**"imagine putting in ram sticks when you can just download them" - Hexakon** x1\n**"DON\'T "sfdjhgksfdj" ME, I AM IN PAIN :NotLikeShift:" - Econ** x1\n**"if i die blame firefox" - KubeBow** x1\n"**It\'s like feeding seagulls but the birds are us and the breadcrumbs are small editor previews" - CV35W** x1\n**"what kind of baguette fuckery is this" - TNTz** x1\n**"i just hid loss in rd and i feel great" - giacomo** x1\n(page 2 of 2)')
+    end
+  else
+    message.channel:send("<@".. message.author.id ..">, your inventory contains:\n" .. invstring .. "(page ".. pagenumber .. " of " .. maxpn .. ")")
+  end
 end
 return command
   
