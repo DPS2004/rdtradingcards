09118825df851d4b5d8e90dbae205f8fd1d7ff27
DPS2004 <iheartscratch@gmail.com>
2021-02-22 19:34:07 -0500

    change usage slightly

--- commands/tell.lua
+++ commands/tell.lua
@@ -2,7 +2,7 @@ local command = {}
 function command.run(message, mt)
   local cmember = message.guild:getMember(message.author)
   if cmember:hasRole(privatestuff.modroleid) then
-    message.guild:getChannel(privatestuff.pullchannelid):send(mt[1])
+    message.guild:getChannel(mt[1]):send(mt[2])
   else
     message.channel:send("haha no, nice try")
   end
