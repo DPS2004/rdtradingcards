62fc881fda2f7da262784def9310da484bdc67b3
DPS2004 <iheartscratch@gmail.com>
2021-04-13 15:21:01 -0400

    reactions added, should be mostly usable now!

--- mainemu.lua
+++ mainemu.lua
@@ -7,6 +7,7 @@ _G["discordia"] = {
   Client = function()
     return {
       on = function(this,trigger, func)
+        io.write("EMU: client.on called\n")
         discordia.ons[trigger] = func
       end,
       run = function()
@@ -15,24 +16,83 @@ _G["discordia"] = {
           
           io.write("waiting for input: ")
           local inp = io.read()
-          if string.lower(string.sub(inp, 0, 4)) == 'msg!' then
-            local messagecontent = string.sub(inp, 5)
+          if string.lower(string.sub(inp, 0, 2)) == 'm!' then
+            local messagecontent = string.sub(inp, 3)
             io.write("message detected: " .. messagecontent.. "\n")
             discordia.ons.messageCreate({
               content = messagecontent,
               author = {
                 id="290582109750427648",
-                name = "DPS2004"
+                name = "DPS2004",
+                mentionString = "<@290582109750427648>",
+                discriminator = "5143"
               },
               channel = {
                 send = function(this, content)
                   --print("sending")
+                  if type(content) ~= "string" then
+                    content = inspect(content)
+                  end
                   io.write("BOT: " .. content .. "\n")
+                  
+                  return {
+                    addReaction = function(self)
+                      io.write("EMU: message:addReaction called. Please set a message id: (this will be asked twice, probably)\n")
+                      local inp = io.read()
+                      self.id = inp --ostor is sus!
+                    end
+                    
+                  }
+                end
+              },
+              guild = {
+                getMember = function()
+                  io.write("EMU: guild.getMember called\n")
+                  return({
+                    hasRole = function()
+                      io.write("EMU: member:hasRole called\n")
+                      return true
+                    end
+                  })
+                  
                 end
               }
             })
-          
           end
+          if string.lower(string.sub(inp, 0, 2)) == 'r!' then
+            local messageid = string.sub(inp, 3)
+            io.write("reaction detected: " .. messageid.. "\n")
+            io.write("what emoji? (✅ or ❌): ")
+            local inp = io.read()
+            discordia.ons.reactionAdd(
+              {
+                emojiName = inp,
+                message = {
+                  id = messageid,
+                  channel = {
+                    send = function(this, content)
+                      --print("sending")
+                      if type(content) ~= "string" then
+                        content = inspect(content)
+                      end
+                      io.write("BOT: " .. content .. "\n")
+                      
+                      return {
+                        addReaction = function(self)
+                          io.write("EMU: message:addReaction called. Please set a message id: (this will be asked twice, probably)\n")
+                          local inp = io.read()
+                          self.id = inp --ostor is sus!
+                        end
+                        
+                      }
+                    end
+                  }
+                }
+              },
+              "290582109750427648"
+            )
+          end
+
         end
         
       end,
@@ -47,9 +107,28 @@ _G["discordia"] = {
   ons = {},
   
   Stopwatch = function()
+    
+    io.write("EMU: Stopwatch created\n")
+      
     return {
       start = function()
-        
+        io.write("EMU: Stopwatch.start called\n")
+      end,
+      getTime = function()
+        io.write("EMU: Stopwatch.getTime called.\n")
+        return {
+          toHours = function()
+            io.write("EMU: Stopwatch.getTime:toHours called. Please enter a value: \n")
+            local inp = io.read()
+            return tonumber(inp)
+          end,
+          toDays = function()
+            io.write("EMU: Stopwatch.getTime:toDays called. Please enter a value: \n")
+            local inp = io.read()
+            return tonumber(inp)
+          end
+          
+        }
       end
     }
   end
