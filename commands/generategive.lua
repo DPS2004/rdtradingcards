22f764041652c72fb5292edc5f5b50c2b7d9573c
RandomGuyJCI <carleisaguirre@gmail.com>
2021-04-15 09:42:50 +0800

    Add the ability to generate and grant multiple tokens/cards

--- commands/generategive.lua
+++ commands/generategive.lua
@@ -3,25 +3,32 @@ function command.run(message, mt)
   print(message.author.name .. " did !generategive")
   local cmember = message.guild:getMember(message.author)
   if cmember:hasRole(privatestuff.modroleid) then
-    if #mt == 2 then
-      local uj = dpf.loadjson("savedata/" .. message.author.id .. ".json",defaultjson)
+    if #mt == 2 or #mt == 3 then
       local uj2f = usernametojson(mt[1])
       if uj2f then
         local uj2 = dpf.loadjson(uj2f,defaultjson)
   
         local curfilename = texttofn(mt[2])
         if curfilename ~= nil then
-
+          local numcards = 1
+          if tonumber(mt[3]) then
+            if tonumber(mt[3]) > 1 then
+              numcards = math.floor(mt[3])
+            end
+          end
           if uj2.inventory[curfilename] == nil then
-            uj2.inventory[curfilename] = 1
+            uj2.inventory[curfilename] = numcards
           else
-            uj2.inventory[curfilename] = uj2.inventory[curfilename] + 1
+            uj2.inventory[curfilename] = uj2.inventory[curfilename] + numcards
           end
           dpf.savejson(uj2f,uj2)
           print("saved user2 json with new card")
-          
+          local isplural = ""
+          if numcards ~= 1 then
+            isplural = "s"
+          end
           message.channel:send {
-            content = 'You have given **' .. fntoname(curfilename) .. '** card to <@' .. uj2.id .. '> .'
+            content = 'You have given ' .. numcards .. ' **' .. fntoname(curfilename) .. '** card' .. isplural .. ' to <@' .. uj2.id .. '> .'
           }
 
         else
@@ -32,7 +39,7 @@ function command.run(message, mt)
         message.channel:send("Sorry, but I could not find a user named " .. mt[1] .. " in the database. Make sure that you have spelled it right, and that they have at least pulled a card to register!")
       end
     else
-      message.channel:send("Sorry, but the c!generategive command expects 2 arguments. Please see c!help for more details.")
+      message.channel:send("Sorry, but the c!generategive command expects 2 or 3 arguments. Please see c!help for more details.")
     end
   else
     message.channel:send("haha no, nice try")
--- commands/granttoken.lua
+++ commands/granttoken.lua
@@ -2,15 +2,28 @@ local command = {}
 function command.run(message, mt)
   local cmember = message.guild:getMember(message.author)
   if cmember:hasRole(privatestuff.modroleid) then
-    local uj = dpf.loadjson("savedata/" .. mt[1] .. ".json",defaultjson)
-    if not uj.tokens then
-      uj.tokens = 1
+    if usernametojson(mt[1]) then
+      local uj = dpf.loadjson(usernametojson(mt[1]),defaultjson)
+      local numtokens = 1
+      if tonumber(mt[2]) then
+        if tonumber(mt[2]) > 1 then
+          numtokens = math.floor(tonumber(mt[2]))
+        end
+      end
+      if not uj.tokens then
+        uj.tokens = numtokens
+      else
+        uj.tokens = uj.tokens + numtokens
+      end
+      dpf.savejson(usernametojson(mt[1]),uj)
+      local isplural = " Token has "
+      if numtokens ~= 1 then
+        isplural = " Tokens have "
+      end
+      message.channel:send(numtokens .. isplural .. "been given to " .. mt[1] .. ".")
     else
-      uj.tokens = uj.tokens + 1
+      message.channel:send("Sorry, but I could not find a user named " .. mt[1] .. " in the database. Make sure that you have spelled it right, and that they have at least pulled a card to register!")
     end
-    dpf.savejson("savedata/" .. mt[1] .. ".json",uj)
-        
-    message.channel:send("token has been given")
   else
     message.channel:send("haha no, nice try")
   end
