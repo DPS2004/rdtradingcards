805f368fabd369fcd81ce5e844cb9368836b7f01
RandomGuyJCI <carleisaguirre@gmail.com>
2021-04-30 21:36:35 +0800

    Add c!renamefile
    god i hope this doesn't break when it's used in the update

---
+++ commands/renamefile.lua
@@ -0,0 +1,45 @@
+
+local command = {}
+function command.run(message, mt)
+  print(message.author.name .. " did !renamefile")
+  local cmember = message.guild:getMember(message.author)
+  if cmember:hasRole(privatestuff.modroleid) then
+    if #mt == 2 then
+      -- why did i even add this if statement
+      if fntoname(mt[1]) or fntoname(mt[2]) or medalfntoname(mt[1]) or medalfntoname(mt[2]) or itemfntoname(mt[1]) or itemfntoname(mt[2]) then
+        for i,v in ipairs(scandir("savedata")) do
+          cuj = dpf.loadjson("savedata/"..v,defaultjson)
+          if cuj.id then
+              if cuj.inventory[mt[1]] then
+                cuj.inventory[mt[2]] = cuj.inventory[mt[1]]
+                cuj.inventory[mt[1]] = nil
+              end
+              if cuj.storage[mt[1]] then
+                cuj.storage[mt[2]] = cuj.inventory[mt[1]]
+                cuj.storage[mt[1]] = nil
+              end
+              if cuj.medals[mt[1]] then
+                cuj.medals[mt[2]] = cuj.medals[mt[1]]
+                cuj.medals[mt[1]] = nil
+              end
+              if cuj.items[mt[1]] then
+                cuj.items[mt[2]] = cuj.items[mt[1]]
+                cuj.items[mt[1]] = nil
+              end
+          end
+          dpf.savejson("savedata/"..v,cuj)
+        end
+        message.channel:send("Renamed " .. mt[1] .. " to " .. mt[2] .. ".")
+      else
+        message.channel:send("Sorry, but " .. mt[1] .. " doesn't exist in the database.")
+      end
+    else
+      message.channel:send("Sorry, but the c!renamefile command expects 2 arguments.")
+    end
+  else
+    
+    message.channel:send('Sorry, but only moderators can use this command!')
+  end
+end
+return command
+  
