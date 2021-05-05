b50ac10f2c68fcb492ee6fd38201d41b59e7012d
RandomGuyJCI <carleisaguirre@gmail.com>
2021-05-05 14:58:02 +0800

    the bean now works in DMs

--- commands/beans.lua
+++ commands/beans.lua
@@ -1,11 +1,7 @@
 local command = {}
 function command.run(message, mt)
   print(message.author.name .. " did !beans")
-  if message.guild.name == "Rhythm Doctor Lounge" then
-    message:addReaction(client:getEmoji("340218056934686732"))
-  else
-    message:addReaction(client:getEmoji("816463414959407115"))
-  end
+  message:addReaction(client:getEmoji("340218056934686732"))
 end
 return command
   
