03fd6b3fd8ea88537a747f3a075b0ed524fc866e
RandomGuyJCI <carleisaguirre@gmail.com>
2021-05-07 02:54:28 +0800

    This was a huge oeuf moment

--- commands/crash.lua
+++ commands/crash.lua
@@ -4,9 +4,6 @@ function command.run(message, mt)
   print("lmao someone did c!crash")
   local cmember = message.guild:getMember(message.author)
   if cmember:hasRole(privatestuff.modroleid) then
-    for i,v in ipairs(scandir("savedata")) do
-      resetclocks()
-    end
     message.channel:send('Time to crash! Here I go! Hopefully this should say something about concatenating a nil value!')
     print("string string stringity string" .. nilvalue)
   else
