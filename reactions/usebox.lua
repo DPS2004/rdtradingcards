370504455f6dd2fafab1a61e9788a88dd0959053
RandomGuyJCI <carleisaguirre@gmail.com>
2021-05-20 00:12:48 +0800

    ddockson moment

--- reactions/usebox.lua
+++ reactions/usebox.lua
@@ -11,46 +11,50 @@ function reaction.run(ef, eom, reaction, userid)
     
     if reaction.emojiName == "✅" then
       print('user1 has accepted')
-      local iptable = {}
-      for k,v in pairs(uj.inventory) do
-        table.insert(iptable, k)
-      end
-      local givecard = iptable[math.random(1,#iptable)]
-      print("user giving " .. givecard)
-      
-      local boxpoolindex = math.random(1,#wj.boxpool)
-      local getcard = wj.boxpool[boxpoolindex]
-      
-      if uj.inventory[getcard] == nil then
-        uj.inventory[getcard] = 1
+      if next(uj.inventory) then
+        local iptable = {}
+        for k,v in pairs(uj.inventory) do
+          table.insert(iptable, k)
+        end
+        local givecard = iptable[math.random(1,#iptable)]
+        print("user giving " .. givecard)
+        
+        local boxpoolindex = math.random(1,#wj.boxpool)
+        local getcard = wj.boxpool[boxpoolindex]
+        
+        if uj.inventory[getcard] == nil then
+          uj.inventory[getcard] = 1
+        else
+          uj.inventory[getcard] = uj.inventory[getcard] + 1
+        end
+        
+        uj.inventory[givecard] = uj.inventory[givecard] - 1
+        
+        if uj.inventory[givecard] == 0 then
+          uj.inventory[givecard] = nil
+        end
+        
+        wj.boxpool[boxpoolindex] = givecard
+        
+        local newmessage = reaction.message.channel:send {
+          content = '<@' .. uj.id .. '> grabs a **' .. fntoname(givecard) .. '** card from their inventory and places it inside the box. As it goes in, a **' .. fntoname(getcard) .. '** card shows up in their pocket!'
+        }
+        
+        if uj.timesusedbox == nil then
+          uj.timesusedbox = 1
+        else
+          uj.timesusedbox = uj.timesusedbox + 1
+        end
+        uj.lastbox = time:toHours()
+        
+        dpf.savejson(ujf,uj)
+        ef[reaction.message.id] = nil
+        
+        dpf.savejson("savedata/events.json",ef)
+        dpf.savejson("savedata/worldsave.json", wj)
       else
-        uj.inventory[getcard] = uj.inventory[getcard] + 1
+        local newmessage = reaction.message.channel:send("An error has occured. Please make sure that you still have a card in your inventory!")
       end
-      
-      uj.inventory[givecard] = uj.inventory[givecard] - 1
-      
-      if uj.inventory[givecard] == 0 then
-        uj.inventory[givecard] = nil
-      end
-      
-      wj.boxpool[boxpoolindex] = givecard
-      
-      local newmessage = reaction.message.channel:send {
-        content = '<@' .. uj.id .. '> grabs a **' .. fntoname(givecard) .. '** card from their inventory and places it inside the box. As it goes in, a **' .. fntoname(getcard) .. '** card shows up in their pocket!'
-      }
-      
-      if uj.timesusedbox == nil then
-        uj.timesusedbox = 1
-      else
-        uj.timesusedbox = uj.timesusedbox + 1
-      end
-      uj.lastbox = time:toHours()
-      
-      dpf.savejson(ujf,uj)
-      ef[reaction.message.id] = nil
-      
-      dpf.savejson("savedata/events.json",ef)
-      dpf.savejson("savedata/worldsave.json", wj)
     end
     if reaction.emojiName == "❌" then
       print('user1 has denied')
