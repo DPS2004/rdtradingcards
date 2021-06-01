15f3634eba6f20516ea3043b3768a630dddf6329
Nocallia <71400240+Nocallia@users.noreply.github.com>
2021-06-01 18:33:08 -0400

    Reference to pronounform

--- commands/pronounlist.lua
+++ commands/pronounlist.lua
@@ -1,6 +1,6 @@
 local command = {}
 function command.run(message, mt)
-  message.channel:send('**Common Pronouns:**\nHe, She, They, It\n\n**Neopronouns:**\nXe, Ze, Sta')
+  message.channel:send('**Common Pronouns:**\nHe, She, They, It\n\n**Neopronouns:**\nXe, Ze, Sta\n\n*If your pronouns are not included, please use c!pronounform')
   print(message.author.name .. " did !pronounlist")
 end
 return command
