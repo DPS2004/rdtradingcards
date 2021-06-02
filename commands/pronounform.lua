c76c20fbcfdc32523c09a914656c6cf451db0421
Nocallia <71400240+Nocallia@users.noreply.github.com>
2021-06-01 20:19:31 -0400

    h

--- commands/pronounform.lua
+++ commands/pronounform.lua
@@ -1,6 +1,6 @@
 local command = {}
 function command.run(message, mt)
-  message.channel:send('Your pronouns not on the RDCard bot? Put your pronouns in this google form and we will look over them!/n**https://forms.gle/nG57xLc8DPHc5G8m9**')
+  message.channel:send('Your pronouns not on the RDCard bot? Put your pronouns in this google form and we will look over them!\n**https://forms.gle/nG57xLc8DPHc5G8m9**')
   print(message.author.name .. " did !pronounform")
 end
 return command
