4b03ef5b7edb02f5585628a0f789bc422cf8e93c
DPS2004 <iheartscratch@gmail.com>
2021-06-07 19:14:48 -0400

    sung lasses

--- commands/smell.lua
+++ commands/smell.lua
@@ -41,7 +41,7 @@ function command.run(message, mt)
       deviltail = "The **Devil Tail** smells like fire and brimstone.",
       scribblednotepad = "The **Scribbled-In Notepad** smells like imagination.",
       okamiiscollar = "**okamii's Collar** smells like midnight.",
-      coolhat = "The **Cool Hat** smells really cool :sunglasses"
+      coolhat = "The **Cool Hat** smells really cool :sunglasses:"
     }
     request = string.lower(request)
     if request == "strange machine" then
