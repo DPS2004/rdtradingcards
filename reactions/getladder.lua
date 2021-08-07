eb35c20dbd0d23d5a38222b457824165aaf9d321
RandomGuyJCI <carleisaguirre@gmail.com>
2021-08-07 18:56:23 +0800

    More reaction code cleanup

--- reactions/getladder.lua
+++ reactions/getladder.lua
@@ -1,40 +1,35 @@
-reaction = {}
+local reaction = {}
 function reaction.run(ef, eom, reaction, userid)
   local ujf = eom.ujf
-  local newequip = eom.newequip
   local uj = dpf.loadjson(ujf, defaultjson)
-  local time = sw:getTime()
-  print("loaded uj")
+  print("Loaded uj")
   local wj = dpf.loadjson("savedata/worldsave.json", defaultworldsave)
-  if uj.id == userid then
-    print('user1 has reacted')
-    
-    if reaction.emojiName == "✅" then
-      print('user1 has accepted')
-      
-      uj.tokens = uj.tokens - 4
-      local newmessage = reaction.message.channel:send {
-        content = 'After depositing 4 **Tokens** and turning the crank, a capsule comes out of the **Strange Machine**. Inside it is the **Ladder!**! You put the **Ladder** in the hole. In addition, the **Strange Machine** seems to have calmed down.'
-      }
-      dpf.savejson(ujf,uj)
-      ef[reaction.message.id] = nil
-      wj.ws = 507 -- see setworldstate.lua
-      dpf.savejson("savedata/worldsave.json", wj)
-      
-      dpf.savejson("savedata/events.json",ef)
-    end
-    if reaction.emojiName == "❌" then
-      print('user1 has denied')
-      
-      local newmessage = reaction.message.channel:send("You decide to not use the **Strange Machine**.")
-      ef[reaction.message.id] = nil
-      dpf.savejson("savedata/events.json",ef)
-      
-      
-    end
-  else
+
+  if uj.id ~= userid then
     print("its not uj1 reacting")
+    return
+  end
+
+  print('user1 has reacted')
+  ef[reaction.message.id] = nil
+  dpf.savejson("savedata/events.json", ef)
+
+  if reaction.emojiName == "✅" then
+    print('user1 has accepted')
+    if uj.tokens < 4 then
+      reaction.message.channel:send("An error has occured. Please make sure that you still have enough tokens!")
+      return
+    end
+    uj.tokens = uj.tokens - 4
+    wj.ws = 507 -- see setworldstate.lua
+    reaction.message.channel:send('After depositing 4 **Tokens** and turning the crank, a capsule comes out of the **Strange Machine**. Inside it is the **Ladder!**! You put the **Ladder** in the hole. In addition, the **Strange Machine** seems to have calmed down.')
+    dpf.savejson(ujf,uj)
+    dpf.savejson("savedata/worldsave.json", wj)
+  end
+
+  if reaction.emojiName == "❌" then
+    print('user1 has denied')
+    reaction.message.channel:send("You decide to not use the **Strange Machine**.")
   end
 end
 return reaction
-  
--- reactions/usehole.lua
+++ reactions/usehole.lua
@@ -2,10 +2,10 @@ local reaction = {}
 function reaction.run(ef, eom, reaction, userid)
   local ujf = eom.ujf
   local uj = dpf.loadjson(ujf, defaultjson)
-  print("loaded uj")
+  print("Loaded uj")
   local wj = dpf.loadjson("savedata/worldsave.json", defaultworldsave)
   if uj.id ~= userid then
-    print("its not uj1 reacting")
+    print("It's not uj1 reacting")
     return
   end
 
@@ -15,6 +15,11 @@ function reaction.run(ef, eom, reaction, userid)
 
   if reaction.emojiName == "✅" then
     print('user1 has accepted')
+    if uj.tokens < 1 then
+      reaction.message.channel:send("An error has occured. Please make sure that you still have a token!")
+      return
+    end
+
     uj.tokens = uj.tokens - 1
     uj.timesused = uj.timesused and uj.timesused + 1 or 1
     uj.tokensdonated = uj.tokensdonated and uj.tokensdonated + 1 or 1
@@ -40,7 +45,7 @@ function reaction.run(ef, eom, reaction, userid)
 
     wj.tokensdonated = wj.tokensdonated + 1
 
-    if  wj.ws >= 501 and wj.ws < 506 and wj.tokensdonated >= (wj.ws - 500) * 5 then
+    if wj.ws >= 501 and wj.ws < 506 and wj.tokensdonated >= (wj.ws - 500) * 5 then
       wj.ws = wj.ws + 1
       reaction.message.channel:send("***The ground rumbles..." .. (wj.ws == 506 and " and so does the Strange Machine***" or "***"))
     end
