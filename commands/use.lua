946469ef2d624095244df598f5e46fb2c5b3320f
Lumina0306 <66207557+Lumina0306@users.noreply.github.com>
2021-08-07 16:27:36 +0900

    machine embed fix
    removed avatar icon

--- commands/use.lua
+++ commands/use.lua
@@ -33,10 +33,6 @@ function command.run(message, mt,bypass)
                     color = 0x85c5ff,
                     title = "Using Strange Machine...",
                     description = 'Will you put two **Tokens** into the **Strange Machine?** (tokens remaining: ' .. uj.tokens .. ')',
-                    footer = {
-                      text =  message.author.name,
-                      icon_url = message.author.avatarURL
-                    }
                   },"usemachine",{})
                 else
                   local newitem = itempt[math.random(#itempt)]
