d601a2e610fe2db7a146c189d8d7264d8723b4c0
DPS2004 <iheartscratch@gmail.com>
2021-08-15 16:55:32 -0400

    fix some errors found by testcards

--- commands/testcards.lua
+++ commands/testcards.lua
@@ -4,11 +4,12 @@ function command.run(message, mt)
     message.channel:send('ok, testing. There are '.. #cdb ..'cards in the database.')
     print(message.author.name .. " did !testcards")
     for i,v in ipairs(cdb) do
+      local emb = v.embed or ""
       message.channel:send {
-        content = 'TESTCARDS: '.. v.name .. ' smells like ' .. getcardsmell(v.filename)
+        content = 'TESTCARDS: '.. v.name .. ' smells like ' .. getcardsmell(v.filename) .. ', ' .. getcarddescription(v.filename).. emb
       }
-      message.channel:send {
-        content = v.embed
+      message.channel:send{
+        file = getcardthumb(v.filename)
       }
     end
   else
