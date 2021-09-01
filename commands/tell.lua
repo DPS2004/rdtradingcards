498881e6f02f97a362b050937d947d21dc3a1906
RandomGuyJCI <carleisaguirre@gmail.com>
2021-09-01 21:47:42 +0800

    be able to use slashes in tell and megaphone

--- commands/tell.lua
+++ commands/tell.lua
@@ -2,7 +2,7 @@ local command = {}
 function command.run(message, mt)
   local cmember = message.guild:getMember(message.author)
   if cmember:hasRole(privatestuff.modroleid) then
-    message.guild:getChannel(mt[1]):send(mt[2])
+    message.guild:getChannel(mt[1]):send(table.concat(mt, "/", 2))
   else
     message.channel:send("haha no, nice try")
   end
--- consumables/megaphone.lua
+++ consumables/megaphone.lua
@@ -3,15 +3,14 @@ local item = {}
 function item.run(uj,ujf,message,mt)
   local cardchannel = privatestuff.cardchannel or '793993844789870603'
   if not uj.conspt then uj.conspt = "none" end
-  if #mt == 2 then
+  if #mt ~= 1 then
     uj.consumables["megaphone"] = uj.consumables["megaphone"] - 1
     if uj.consumables["megaphone"] == 0 then uj.consumables["megaphone"] = nil end
     uj.timesitemused = uj.timesitemused and uj.timesitemused + 1 or 1
     
-    local text = mt[2]:gsub("<","")
-    text = text:gsub(">","")
+    local text = table.concat(mt, "/", 2):gsub("[<>]", "")
     dpf.savejson(ujf, uj)
-    client:getChannel(cardchannel):send("A message comes through the **Megaphone**:\n" .. text)
+    client:getChannel(cardchannel):send("A message comes through the **Megaphone**:\n" .. text):hideEmbeds()
     if message.guild then message:delete() end
   else
     message.channel:send("The megaphone was not used. Please attach a message with c!use megaphone/(YOUR MESSAGE HERE)")
