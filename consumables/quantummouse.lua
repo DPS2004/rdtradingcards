11443e3590756e5bc8cce4f03f7e50f5ff45d713
RandomGuyJCI <carleisaguirre@gmail.com>
2021-09-23 09:24:43 +0800

    add quantum mouse consumable placeholder

---
+++ consumables/quantummouse.lua
@@ -0,0 +1,17 @@
+local item = {}
+
+function item.run(uj, ujf, message, mt)
+  if not uj.conspt then uj.conspt = "none" end
+  if uj.conspt == "none" then
+    uj.consumables["quantummouse"] = uj.consumables["quantummouse"] - 1
+    if uj.consumables["quantummouse"] == 0 then uj.consumables["quantummouse"] = nil end
+    uj.timesitemused = uj.timesitemused and uj.timesitemused + 1 or 1
+    
+    uj.conspt = "fixedmouse"
+    dpf.savejson(ujf, uj)
+    message.channel:send("you put down the ***quantum mouse*** or smth. it squeaks lol")
+  else
+    message.channel:send("You already have a pull affecting item in use! You decide against using the **Quantum Mouse** for now.")
+  end
+end
+return item
