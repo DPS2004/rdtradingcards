17c060b06da18b38d6f38f0f0635a106ea3dab96
RandomGuyJCI <carleisaguirre@gmail.com>
2022-03-22 12:51:08 +0800

    betterify equip.lua (i think)

--- commands/equip.lua
+++ commands/equip.lua
@@ -21,48 +21,8 @@ function command.run(message, mt)
     end
     
     dpf.savejson("savedata/" .. message.author.id .. ".json",uj)
-    if uj.lastequip + 6 <= time:toHours()then
-      local request = mt[1]
-      local curfilename = itemtexttofn(request)
-      if curfilename then
-        if uj.items[curfilename] then
-          if uj.equipped ~= curfilename then
-            --woo hoo
-            print(uj.equipped)
-            if not uj.skipprompts then
-              ynbuttons(message,"Would you like to change your equipped item from **" .. itemdb[uj.equipped].name .. "** to **" .. itemdb[curfilename].name .. "**? This can be done once every 6 hours.","equip",{newequip = curfilename})
-            else
-              uj.equipped = curfilename
-              message.channel:send("<@" .. uj.id .. "> successfully set **" .. itemdb[curfilename].name .. "** as "..uj.pronouns["their"].." equipped item.")
-              uj.lastequip = time:toHours()
-              dpf.savejson(ujf,uj)
-              print('saved equipped as ' .. curfilename)
 
-              if uj.sodapt and uj.sodapt.equip then
-                uj.lastequip = uj.lastequip + uj.sodapt.equip
-                uj.sodapt.equip = nil
-                if uj.sodapt == {} then uj.sodapt = nil end
-              end
-            end
-          else
-            message.channel:send("You already have the **" .. itemdb[curfilename].name .. "** item equipped!")
-          end
-        else
-          if nopeeking then
-            message.channel:send("Sorry, but I either could not find the " .. request .. " item in the database, or you do not have it. Make sure that you spelled it right!")
-          else
-            message.channel:send("Sorry, but you don't have the **" .. itemdb[curfilename].name .. "** item.")
-          end
-        end
-        
-      else
-        if nopeeking then
-          message.channel:send("Sorry, but I either could not find the " .. request .. " item in the database, or you do not have it. Make sure that you spelled it right!")
-        else
-          message.channel:send("Sorry, but I could not find the " .. request .. " item in the database. Make sure that you spelled it right!")
-        end
-      end
-    else
+    if not uj.lastequip + 6 <= time:toHours() then
       --extremely jank implementation, please make this cleaner if possible
       local minutesleft = math.ceil(uj.lastequip * 60 - time:toMinutes() + 360.00)
       local durationtext = ""
@@ -82,6 +42,51 @@ function command.run(message, mt)
         end
       end
       message.channel:send('Please wait ' .. durationtext .. ' before changing your equipped item. ')
+      return
+    end
+
+    local request = mt[1]
+    local curfilename = itemtexttofn(request)
+    
+    if not curfilename then
+      if nopeeking then
+        message.channel:send("Sorry, but I either could not find the " .. request .. " item in the database, or you do not have it. Make sure that you spelled it right!")
+      else
+        message.channel:send("Sorry, but I could not find the " .. request .. " item in the database. Make sure that you spelled it right!")
+      end
+      return
+    end
+
+    if not uj.items[curfilename] then
+      if nopeeking then
+        message.channel:send("Sorry, but I either could not find the " .. request .. " item in the database, or you do not have it. Make sure that you spelled it right!")
+      else
+        message.channel:send("Sorry, but you don't have the **" .. itemdb[curfilename].name .. "** item.")
+      end
+      return
+    end
+
+    if uj.equipped == curfilename then
+      message.channel:send("You already have the **" .. itemdb[curfilename].name .. "** item equipped!")
+      return
+    end
+
+    --woo hoo
+    print(uj.equipped)
+    if not uj.skipprompts then
+      ynbuttons(message,"Would you like to change your equipped item from **" .. itemdb[uj.equipped].name .. "** to **" .. itemdb[curfilename].name .. "**? This can be done once every 6 hours.","equip",{newequip = curfilename})
+    else
+      uj.equipped = curfilename
+      message.channel:send("<@" .. uj.id .. "> successfully set **" .. itemdb[curfilename].name .. "** as "..uj.pronouns["their"].." equipped item.")
+      uj.lastequip = time:toHours()
+      dpf.savejson(ujf,uj)
+      print('saved equipped as ' .. curfilename)
+
+      if uj.sodapt and uj.sodapt.equip then
+        uj.lastequip = uj.lastequip + uj.sodapt.equip
+        uj.sodapt.equip = nil
+        if uj.sodapt == {} then uj.sodapt = nil end
+      end
     end
           
   else
