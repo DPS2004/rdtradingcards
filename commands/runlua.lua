fec6a33aec6c93dfc4282f83e141fee600e093bb
RandomGuyJCI <carleisaguirre@gmail.com>
2022-06-27 03:27:44 +0800

    better error handling for runlua

--- commands/runlua.lua
+++ commands/runlua.lua
@@ -6,8 +6,17 @@ function command.run(message, mt)
   if cmember:hasRole(privatestuff.modroleid) then
     message.channel:send('Ok, running!')
     local request = table.concat(mt, "/")
-    local rfunc = assert(loadstring(request))
-    rfunc()
+
+    local status, err = pcall(function()
+      local rfunc = assert(load(request))
+      rfunc()
+    end)
+
+    if not status then
+      message.channel:send('Oops! An error has occured. Error message: ```' .. err .. '```')
+    else
+      message.channel:send('Success!')
+    end
   else
     message.channel:send('Sorry, but only moderators can use this command!')
   end
