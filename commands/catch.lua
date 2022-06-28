1a20f844b639631610398b966d285848367a4ece
RandomGuyJCI <carleisaguirre@gmail.com>
2022-06-28 12:07:31 +0800

    add the ability to throw consumables

--- commands/catch.lua
+++ commands/catch.lua
@@ -3,7 +3,7 @@ function command.run(message, mt)
   print(message.author.name .. " did !catch")
 
   if not message.guild then
-    message.channel:send("Sorry, but you cannot catch cards in DMs!")
+    message.channel:send("Sorry, but you cannot catch in DMs!")
     return
   end
 
@@ -13,39 +13,47 @@ function command.run(message, mt)
   end
 
   local uj = dpf.loadjson("savedata/" .. message.author.id .. ".json", defaultjson)
-  local tj = dpf.loadjson("savedata/throwncards.json", {})
+  local tj = dpf.loadjson("savedata/thrown.json", {})
 
-  local curfilename = texttofn(mt[1])
-  
+  local cardfilename, consfilename = texttofn(mt[1]), constexttofn(mt[1])
+  local curfilename = cardfilename or consfilename
+  print(curfilename)
+
   if not curfilename then
     if nopeeking then
-      message.channel:send("Sorry, but I either could not find the " .. mt[1] .. " card in the database, or it's not been thrown. Make sure that you spelled it right!")
+      message.channel:send("Sorry, but I either could not find " .. mt[1] .. " in the database, or it's not been thrown. Make sure that you spelled it right!")
     else
-      message.channel:send("Sorry, but I could not find the " .. mt[1] .. " card in the database. Make sure that you spelled it right!")
+      message.channel:send("Sorry, but I could not find " .. mt[1] .. " in the database. Make sure that you spelled it right!")
     end
     return
   end
 
-  if not tj[curfilename] then
-    print("user doesnt have card")
+  local caughtname = cardfilename and cdb[cardfilename].name or consdb[consfilename].name
+
+  if not (tj[curfilename]) then
+    print("user doesnt have item")
     if nopeeking then
-      message.channel:send("Sorry, but I either could not find the " .. mt[1] .. " card in the database, or it's not been thrown. Make sure that you spelled it right!")
+      message.channel:send("Sorry, but I either could not find " .. mt[1] .. " in the database, or it's not been thrown. Make sure that you spelled it right!")
     else
-      message.channel:send("Sorry, but the **" .. cdb[curfilename].name .. "** card has not been thrown!")
+      message.channel:send("Sorry, but **" .. caughtname .. "** has not been thrown!")
     end
     return
   end
 
-  uj.inventory[curfilename] = uj.inventory[curfilename] and uj.inventory[curfilename] + 1 or 1
+  if cardfilename then
+    uj.inventory[cardfilename] = uj.inventory[cardfilename] and uj.inventory[cardfilename] + 1 or 1
+  else
+    uj.consumables[consfilename] = uj.consumables[consfilename] and uj.consumables[consfilename] + 1 or 1
+  end
   uj.timescaught = uj.timescaught and uj.timescaught + 1 or 1
   client:emit(tj[curfilename][1])
   table.remove(tj[curfilename], 1)
   if not next(tj[curfilename]) then tj[curfilename] = nil end
 
   dpf.savejson("savedata/" .. message.author.id .. ".json",uj)
-  dpf.savejson("savedata/throwncards.json", tj)
+  dpf.savejson("savedata/thrown.json", tj)
 
-  message.channel:send(message.author.mentionString .. " has caught a **" .. cdb[curfilename].name .. "** card! The **" .. cdb[curfilename].name .. "** card has been added to " .. uj.pronouns["their"] .. " inventory.")
+  local item = cardfilename and "card" or "item"
+  message.channel:send(message.author.mentionString .. " has caught a **" .. caughtname .. "** " .. item .. "! The **" .. caughtname .. "** " .. item .. " has been added to " .. uj.pronouns["their"] .. " inventory.")
 end
 return command
-  
--- commands/throw.lua
+++ commands/throw.lua
@@ -5,7 +5,7 @@ function command.run(message, mt)
   local time = sw:getTime()
   local timeout = 20
   if not message.guild then
-    message.channel:send("Sorry, but you cannot throw cards in DMs!")
+    message.channel:send("Sorry, but you cannot throw in DMs!")
     return
   end
 
@@ -16,44 +16,52 @@ function command.run(message, mt)
 
   local uj = dpf.loadjson("savedata/" .. message.author.id .. ".json", defaultjson)
 
-  local curfilename = texttofn(mt[1])
+  local cardfilename, consfilename = texttofn(mt[1]), constexttofn(mt[1])
+  local curfilename = cardfilename or consfilename
   
-  if not curfilename then
+  if not (curfilename) then
     if nopeeking then
-      message.channel:send("Sorry, but I either could not find the " .. mt[1] .. " card in the database, or you do not have it. Make sure that you spelled it right!")
+      message.channel:send("Sorry, but I either could not find " .. mt[1] .. " in the database, or you do not have it. Make sure that you spelled it right!")
     else
-      message.channel:send("Sorry, but I could not find the " .. mt[1] .. " card in the database. Make sure that you spelled it right!")
+      message.channel:send("Sorry, but I could not find " .. mt[1] .. " in the database. Make sure that you spelled it right!")
     end
     return
   end
 
-  if not uj.inventory[curfilename] then
-    print("user doesnt have card")
+  local thrownname = cardfilename and cdb[cardfilename].name or consdb[consfilename].name
+
+  if not (cardfilename and uj.inventory[cardfilename] or uj.consumables[consfilename]) then
+    print("user doesnt have item")
     if nopeeking then
-      message.channel:send("Sorry, but I either could not find the " .. mt[1] .. " card in the database, or you do not have it. Make sure that you spelled it right!")
+      message.channel:send("Sorry, but I either could not find " .. mt[1] .. " in the database, or you do not have it. Make sure that you spelled it right!")
     else
-      message.channel:send("Sorry, but you don't have the **" .. cdb[curfilename].name .. "** card in your inventory.")
+      message.channel:send("Sorry, but you don't have **" .. thrownname .. "** in your inventory.")
     end
     return
   end
 
-  message.channel:send(trf("throw",{ping = message.author.mentionString, card = cdb[curfilename].name}) .. " Type this command within " .. timeout .. " seconds to catch it!\n`c!catch " .. curfilename .. "`")
+  message.channel:send(trf("throw",{ping = message.author.mentionString, name = thrownname}) .. " Type this command within " .. timeout .. " seconds to catch it!\n`c!catch " .. (curfilename) .. "`")
   
   uj = dpf.loadjson("savedata/" .. message.author.id .. ".json", defaultjson)
-  uj.inventory[curfilename] = uj.inventory[curfilename] - 1
-  if uj.inventory[curfilename] == 0 then uj.inventory[curfilename] = nil end
+  if cardfilename then
+    uj.inventory[cardfilename] = uj.inventory[cardfilename] - 1
+    if uj.inventory[cardfilename] == 0 then uj.inventory[cardfilename] = nil end
+  else
+    uj.consumables[consfilename] = uj.consumables[consfilename] - 1
+    if uj.consumables[consfilename] == 0 then uj.consumables[consfilename] = nil end
+  end
   uj.timesthrown = uj.timesthrown and uj.timesthrown + 1 or 1
 
-  local tj = dpf.loadjson("savedata/throwncards.json", {})
+  local tj = dpf.loadjson("savedata/thrown.json", {})
   if not tj[curfilename] then tj[curfilename] = {tostring(time:toHours())} else table.insert(tj[curfilename], tostring(time:toHours())) end
 
   dpf.savejson("savedata/" .. message.author.id .. ".json",uj)
-  print("user had card, removed from original user")
-  dpf.savejson("savedata/throwncards.json", tj)
+  print("user had item, removed from original user")
+  dpf.savejson("savedata/thrown.json", tj)
 
   if not client:waitFor(tostring(time:toHours()), timeout * 1000) then
-    print(curfilename .. " card hasn't been caught!")
-    tj = dpf.loadjson("savedata/throwncards.json", {})
+    print((curfilename) .. " hasn't been caught!")
+    tj = dpf.loadjson("savedata/thrown.json", {})
     uj = dpf.loadjson("savedata/" .. message.author.id .. ".json", defaultjson)
 
     for x = #tj[curfilename], 1, -1 do
@@ -63,13 +71,16 @@ function command.run(message, mt)
       end
     end
 
-    uj.inventory[curfilename] = uj.inventory[curfilename] and uj.inventory[curfilename] + 1 or 1
+    if cardfilename then
+      uj.inventory[cardfilename] = uj.inventory[cardfilename] and uj.inventory[cardfilename] + 1 or 1
+    else 
+      uj.consumables[consfilename] = uj.consumables[consfilename] and uj.consumables[consfilename] + 1 or 1
+    end
 
     if not next(tj[curfilename]) then tj[curfilename] = nil end
     dpf.savejson("savedata/" .. message.author.id .. ".json",uj)
-    dpf.savejson("savedata/throwncards.json", tj)
-    message.channel:send(trf("fall",{card = cdb[curfilename].name}) .. message.author.mentionString .. " picked it up and put it back in " .. uj.pronouns["their"] .. " inventory.")
+    dpf.savejson("savedata/thrown.json", tj)
+    message.channel:send(trf("fall",{name = thrownname, item = cardfilename and "card" or "item"}) .. message.author.mentionString .. " picked it up and put it back in " .. uj.pronouns["their"] .. " inventory.")
   end
 end
 return command
-  
