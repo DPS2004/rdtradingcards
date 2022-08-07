b76ce726b12671184c9cf0d09ec390713c12b896
sjk <sungntung@gmail.com>
2022-08-08 08:06:00 +0900

    Add -s suffix to fullinv and fullstorage

--- commands/fullinventory.lua
+++ commands/fullinventory.lua
@@ -3,13 +3,26 @@ function command.run(message, mt)
   print(message.author.name .. " did !fullinventory")
   local filename = "savedata/" .. message.author.id .. ".json"
 
-  if not (mt[1] == "") then
-    filename = usernametojson(mt[1])
+  local enableShortNames = false
+
+  args = {}
+  for substring in mt[1]:gmatch("%S+") do
+    table.insert(args, substring)
+  end
+
+  if not (args[1] == nil or args[1] == "-s") then
+    filename = usernametojson(args[1])
   end
 
   if not filename then
     message.channel:send("Sorry, but I could not find a user named " .. mt[1] .. " in the database. Make sure that you have spelled it right, and that they have at least pulled a card to register!")
     return
+  end
+
+  for index, value in ipairs(args) do
+    if value == "-s" then
+      enableShortNames = true
+    end
   end
 
   message:addReaction("✅")
@@ -22,7 +35,11 @@ function command.run(message, mt)
   local titlestring = 'Full Inventory'
   local invstring = ''
   local previnvstring = ''
-  for k,v in pairs(uj.inventory) do table.insert(invtable, "**" .. (cdb[k].name or k) .. "** x" .. v .. "\n") end
+  if enableShortNames == true then
+    for k,v in pairs(uj.inventory) do table.insert(invtable, "**" .. (cdb[k].name or k) .. "** x" .. v .. " (" .. k.. ")\n") end
+  else
+    for k,v in pairs(uj.inventory) do table.insert(invtable, "**" .. (cdb[k].name or k) .. "** x" .. v .. "\n") end
+  end
   table.sort(invtable)
   for i = 1, numkey do
     invstring = invstring .. invtable[i]
