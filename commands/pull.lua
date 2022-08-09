e76791325e8d2948c17081ee4e8e34f6f33a18b4
MyseIf <helluser0@gmail.com>
2022-08-09 13:01:10 +0700

    rework boosters

--- commands/pull.lua
+++ commands/pull.lua
@@ -68,8 +68,13 @@ local time = sw:getTime()
   else
     if uj.conspt == "sbubby" then
       pulledcards = { "sandwich" }
+    elseif uj.conspt:sub(1, 6) == "season" then
+      pulledcards = {}
+      table.insert(pulledcards, constable[uj.conspt][math.random(#constable[uj.conspt])])
+      table.insert(pulledcards, constable[uj.conspt][math.random(#constable[uj.conspt])])
+      table.insert(pulledcards, constable[uj.conspt][math.random(#constable[uj.conspt])])
     else
-      pulledcards= { constable[uj.conspt][math.random(#constable[uj.conspt])] }
+      pulledcards = { constable[uj.conspt][math.random(#constable[uj.conspt])] }
     end
     if uj.conspt == "quantummouse" then
       table.insert(pulledcards, constable["quantummouse"][math.random(#constable["quantummouse"])])
--- consumables/seasonbooster.lua
+++ consumables/seasonbooster.lua
@@ -13,9 +13,9 @@ function item.run(uj, ujf, message, mt, interaction, fn)
 
     uj.conspt = "season" .. season
     replying:reply(itemtext)
-    local randtime = math.random(4, 8)
+    --[[local randtime = math.random(4, 8)
     uj.lastpull = uj.lastpull - randtime
-    message:reply('Also, your pull cooldown was decreased by ' .. randtime .. ' hours!')
+    message:reply('Also, your pull cooldown was decreased by ' .. randtime .. ' hours!')]]--
     dpf.savejson(ujf, uj)
   else
     replying:reply("You already have a pull affecting item in use! You decide against using the **" .. itemname .. "** for now.")
