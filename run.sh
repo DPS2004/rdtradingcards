5147e44c426c49c496b0cbd1a27d8cff913bb7b6
sjk <sungntung@gmail.com>
2022-08-25 08:23:21 +0900

    Add .sh files for Mac/Linux

---
+++ run.sh
@@ -0,0 +1,7 @@
+if [ ! -d savedata ]; then mkdir savedata; fi
+if [ ! -d vips_out ]; then mkdir vips_out; fi
+if [ ! -d vips_out/cache ]; then mkdir vips_out/cache; fi
+if [ ! -d vips_out/cache/shop ]; then mkdir vips_out/cache/shop; fi
+if [ ! -d vips_out/cache/items ]; then mkdir vips_out/cache/items; fi
+if [ ! -d vips_out/cache/cards ]; then mkdir vips_out/cache/cards; fi
+luvit main.lua
