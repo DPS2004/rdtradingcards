96d133c4883336590f31bf465cc5fd3742553704
sjk <sungntung@gmail.com>
2022-08-25 09:24:19 +0900

    Use -p flag
    Co-authored-by: huantian <davidtianli@gmail.com>

--- run.sh
+++ run.sh
@@ -1,9 +1,4 @@
 #!/usr/bin/env bash
 
-if [ ! -d savedata ]; then mkdir savedata; fi
-if [ ! -d vips_out ]; then mkdir vips_out; fi
-if [ ! -d vips_out/cache ]; then mkdir vips_out/cache; fi
-if [ ! -d vips_out/cache/shop ]; then mkdir vips_out/cache/shop; fi
-if [ ! -d vips_out/cache/items ]; then mkdir vips_out/cache/items; fi
-if [ ! -d vips_out/cache/cards ]; then mkdir vips_out/cache/cards; fi
+mkdir -p savedata vips_out/cache/{shop,items,cards}
 luvit main.lua
